d1014fbd0fe9d02b7bca01be4577b6f743dfc9d6
zook0320 <zook020320@gmail.com>
2023-11-02 11:41:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+function TodoContainer({ todo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="user-card">
+      <div>{todo.List}</div>
     </div>
   );
 }
+
+const App = () => {
+  const [todoList, setTodoList] = useState([
+    { id: 1, List: 'react를 배워봅시다.' }
+  ]);
+
+  const [List, setList] = useState('');
+
+  const addClickHandler = () => {
+    if (List) {
+      const newList = {
+        id: todoList.length + 1,
+        List: List,
+      };
+
+      setTodoList([...todoList, newList]);
+      setList('');
+    }
+  };
+
+  return (
+    <div>
+      <div className="input">
+        <input
+          value={List}
+          onChange={(e) => setList(e.target.value)}
+        />
+        <button onClick={addClickHandler} >
+          추가하기
+        </button>
+      </div>
+      <h1 className="h1">Todo List</h1>
+      <div className="app-container">
+        {todoList.reverse().map((todo) => {
+          return <TodoContainer todo={todo} key={todo.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default App;
